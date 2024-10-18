ede4b6620f008636500aac8858a0203bacdf051f
PhamTien-363296 <phamnguyenthuytien3007@gmail.com>
2024-10-18 15:22:36 +0700

    thiet lap

---
+++ backend/db/connectMongoDB.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+
+const connectMongoDB = async () => {
+    try{
+        const conn = await mongoose.connect("mongodb://localhost:27017")
+        console.log(`MongoDB connected: ${conn.connection.host}`)
+    }   catch (error){
+        console.error(`Error connection to mongoDB: ${error.message}`)
+        process.exit(1)
+    }
+}
+
+export default connectMongoDB
