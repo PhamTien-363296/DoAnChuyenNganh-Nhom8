23de436fa622e60ed3cc58f0e6a1c0ed290b7050
vhminhthu <vhminhthu824@gmail.com>
2024-10-18 16:13:36 +0700

    thietlap2

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,19 @@
 import express from "express"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
-
-
-
 import connectMongoDB from "./db/connectMongoDB.js"
 
-
 dotenv.config()
-
 
 const app = express()
 const PORT = process.env.PORT || 5000
 
-
-app.use(express.urlencoded({ extended: true})) //to parse form data
-
+app.use(express.urlencoded({ extended: true })) //to parse form data
 app.use(cookieParser())
-
-
-
 
 app.get("/", (req, res) => {
     res.send("Welcome to the Home Page");
 });
-
-
 
 app.listen(5000, ()=>{
     console.log(`Server is running on port ${PORT}`)
