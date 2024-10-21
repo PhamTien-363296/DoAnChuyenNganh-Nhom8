95927be801d79cb16752dcc1fb05b9862cf4919b
vhminhthu <vhminhthu824@gmail.com>
2024-10-21 17:26:03 +0700

    routes2

--- frontend/src/components/pages/sidebar/Tinnhan.jsx
+++ frontend/src/components/pages/sidebar/Tinnhan.jsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 export default function Tinnhan() {
     return (
         <div>Tinnhan</div>
--- frontend/src/components/pages/sidebar/Trangchu.jsx
+++ frontend/src/components/pages/sidebar/Trangchu.jsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 export default function Trangchu() {
     return (
         <div>Trangchu</div>
