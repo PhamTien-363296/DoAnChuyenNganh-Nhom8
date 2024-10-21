ed3198b1635ae3eb4c3b7f7772a26e1b0e0bde84
vhminhthu <vhminhthu824@gmail.com>
2024-10-21 17:48:10 +0700

    routes4

--- frontend/src/components/pages/sidebar/Tintuc.jsx
+++ frontend/src/components/pages/sidebar/Tintuc.jsx
@@ -1,9 +1,5 @@
-<<<<<<< HEAD:frontend/src/components/pages/sidebar/Tintuc.jsx
-=======
-
->>>>>>> f59bae0d878370cef43ca39e5ca5f431782a84be:frontend/src/components/pages/Tintuc.jsx
 export default function Tintuc() {
     return (
-        <div>Tintuc</div>
+        <div>Tintuc 2</div>
     )
 }
--- frontend/src/components/pages/sidebar/Trangchu.jsx
+++ frontend/src/components/pages/sidebar/Trangchu.jsx
@@ -1,8 +1,3 @@
-<<<<<<< HEAD:frontend/src/components/pages/sidebar/Trangchu.jsx
-=======
-
-
->>>>>>> f59bae0d878370cef43ca39e5ca5f431782a84be:frontend/src/components/pages/Trangchu.jsx
 export default function Trangchu() {
     return (
         <div>Trangchu</div>
