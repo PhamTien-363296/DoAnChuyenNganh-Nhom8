1e03876b776092ed538ba2e1749529f48c4594e8
vhminhthu <vhminhthu824@gmail.com>
2024-10-21 17:18:48 +0700

    routes

--- frontend/src/components/common/Sidebar.jsx
+++ frontend/src/components/common/Sidebar.jsx
@@ -1,6 +1,41 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 const Sidebar = () => {
+  const [hienThiThongBao, setHienThiThongBao] = useState("");
+
+  const xulyThongBao = () => {
+    setHienThiThongBao("Hienthi");
+  }
+
   return (
-    <div> Day la Sidebar</div>
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/tintuc">Tin Tức</Link>
+          </li>
+          <li>
+            <Link to="/" onClick={() => xulyThongBao()}>Thông Báo</Link>
+          </li>
+          <li>
+            <Link to="/tinnhan">Tin Nhắn</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <div>
+        {hienThiThongBao === "Hienthi" && (
+          <div id="Thongbao"  style={{ backgroundColor: 'lightyellow'}}>
+            <h2>Thông báo</h2>
+            <p>Đây là thông báo</p>
+          </div>
+        )}
+      </div>
+    </>
   )
 }
 
